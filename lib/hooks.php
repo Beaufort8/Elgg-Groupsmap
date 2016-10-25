0f4f18a928cbc8ccafc14cb3644f408bb00fc3da
Pascal Orth <orth@beaufort8.de>
2016-10-25 19:34:17 +0200

    compatibility with group tools filter menu

--- lib/hooks.php
+++ lib/hooks.php
@@ -18,19 +18,30 @@
 function add_groups_map_tab($hook, $type, $menu, $params) {
     if(elgg_is_active_plugin('amap_maps_api') && amap_ma_check_if_add_tab_on_entity_page('groupsmap')) {
         if(elgg_in_context('groups')) {
-            $filter_context = $params['filter_context'];
-            $options = array(
-                'name' => 'groupsmap',
-                'text' => elgg_echo("groupsmap:menu"),
-                'href' => "groupsmap",
-                'priority' => '600',
-            );	
-            if ($filter_context == 'groupsmap') {
-                $options['selected'] = true;
-            }
-            $menu[] = ElggMenuItem::factory($options);		
-        }
-        return $menu;
-    }
+			// compatibility with group tools filter menu
+			$contextedit = false;
+			foreach($menu as $item){
+				echo $item->getName(),"<br>";
+	
+				if(strpos($item->getName(),'tools')!==false){
+					$contextedit = true;
+					break;
+				}
+			}
+			if(!$contextedit){
+				$filter_context = $params['filter_context'];
+				$options = array(
+					'name' => 'groupsmap',
+					'text' => elgg_echo("groupsmap:menu"),
+					'href' => "groupsmap",
+					'priority' => '600',
+				);	
+				if ($filter_context == 'groupsmap') {
+					$options['selected'] = true;
+				}
+				$menu[] = ElggMenuItem::factory($options);		
+			}
+			return $menu;
+		}
+	}
 }
-
